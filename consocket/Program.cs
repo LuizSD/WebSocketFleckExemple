788fdb670ea05ae53befc1ae903da8a5618bcc92
Felipe Coelho <felipecrleandro@gmail.com>
2017-04-13 16:18:03 -0300

    i exemple of websocket with fleck

---
+++ consocket/Program.cs
@@ -0,0 +1,24 @@
+﻿using Fleck;
+using System;
+
+namespace consocket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var server = new WebSocketServer("ws://0.0.0.0:8181");
+            server.Start(socket =>
+            {
+                socket.OnOpen = () => Console.WriteLine("Open!");
+                socket.OnMessage = message => { Console.WriteLine(message); };
+            });
+
+            Console.ReadKey();
+
+
+        
+}
+    }
+}
